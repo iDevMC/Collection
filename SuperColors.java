59fa57326845450084de9635e55a7674707f24c1
Mick <47728875+iDevMC@users.noreply.github.com>
2021-01-24 13:40:55 -0500

    Create SuperColors.java

---
+++ SuperColors.java
@@ -0,0 +1,19 @@
+/**
+ * Colors are not perfectly their normal RGB value. Only the values I like.
+ */
+public class SuperColors {
+
+	public static final Color DARK_PURPLE  = new Color(49, 4, 74);
+	public static final Color LIGHT_PURPLE = new Color(219, 13, 255);
+	public static final Color PURPLE       = new Color(136, 8, 158);
+
+	public static final Color GOLD         = new Color(140, 123, 22);
+	public static final Color LIME         = new Color(43, 255, 0);
+	public static final Color RED          = new Color(255, 0, 0);
+	
+	public static final Color ORANGE       = new Color(255, 100, 0);
+
+	public static final Color BLACK        = new Color(0, 0, 0);
+	public static final Color WHITE        = new Color(255, 255, 255);
+
+}
